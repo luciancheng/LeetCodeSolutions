4e6a3b4f396b4b18044acb99728fc01ffb667351
Lucian Cheng <121974540+luciancheng@users.noreply.github.com>
2024-06-17 14:35:08 -0400

    Create Solution.py

---
+++ 0547. Number of Provinces/Solution.py
@@ -0,0 +1,39 @@
+class UnionFind():
+    def __init__(self, n):
+        self.parents = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def find(self, x):
+        while self.parents[x] != x:
+            self.parents[x] = self.parents[self.parents[x]]
+            x = self.parents[x]
+        return x
+    
+    def union(self, x, y):
+        p1, p2 = self.find(x), self.find(y)
+
+        if p1 == p2:
+            return False
+        if self.rank[p1] > self.rank[p2]:
+            self.parents[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.parents[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        UF = UnionFind(n)
+
+        for i in range(n):
+            for j in range(n):
+                if (isConnected[i][j]):
+                    UF.union(i, j)
+        
+        s = set()
+        for i in range(n):
+            s.add(UF.find(i))
+
+        return len(s)
