b228b5a2c1293bf816da1d8e83f880ff19590030
Lucian Cheng <121974540+luciancheng@users.noreply.github.com>
2024-08-28 11:28:29 -0400

    Update Solution.cpp

--- 0076. Minimum Window Substring/Solution.cpp
+++ 0076. Minimum Window Substring/Solution.cpp
@@ -5,9 +5,11 @@ public:
         int n = t.size();
         string ans = "";
 
+        // get the occurences of each letter in t 
         unordered_map<char, int> mp;
         for (char i : t) mp[i]++;
 
+        // count represents if we have any valid letters in the window that can be used to form t
         int count = 0;
 
         int l = 0;
@@ -26,6 +28,7 @@ public:
 
             // shrink the window and save the substring if applicable
             // run this loop while all letters of t are fully covered, since the max value in the hashmap will be <= 0
+            // all values in the map are <= 0, which means that the important letters are currently covered in the substring window
             while (findMax(mp) <= 0) {
                 if (mp.count(s[l]) != 0 && mp[s[l]] == 0) {
                     if (ans.size() == 0) {
@@ -36,7 +39,8 @@ public:
                         }
                     }
                 }
-                
+
+                // if the letter at the left bound was an important letter, then remove this letter from the current window map and decrement the count
                 if (mp.count(s[l]) != 0) {
                     mp[s[l]]++;
                     count--;
@@ -48,6 +52,7 @@ public:
         return ans;
     }
 
+    // find the maximum occurence of important letters from t, we need the maximum to be 0, which means that all letters in t are covered in the substring window
     int findMax(const unordered_map<char, int> & mp) {
         int maxNum = INT_MIN;
         for (auto i : mp) {
